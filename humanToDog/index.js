ad3aa3e2630160f70cde0eeb8ef9127b0dfb3b92
HyperGamer <mykola.stankov07@gmail.com>
2026-03-29 15:40:39 +0300

    commit

---
+++ humanToDog/index.js
@@ -0,0 +1,4 @@
+var dogAge = prompt("Enter your age:");
+var humanAge = (dogAge - 2) * 4 + 21;
+
+alert("Your age as a dog is: " + humanAge);
---
+++ moneyCounter/index.js
@@ -0,0 +1,4 @@
+function getMilk(money) {   
+  console.log("leaveHouse");
+}
+getMilk(10)
