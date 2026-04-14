1640e70c849e09c62cc9c0dfd94339f87ab1ac48
HyperGamer <mykola.stankov07@gmail.com>
2026-04-14 14:07:49 +0300

    asadsac

---
+++ jQuery/index.js
@@ -0,0 +1,4 @@
+$("img").attr("src", "https://i.pinimg.com/1200x/68/c0/39/68c0392f474997b4d2937e571cf7a665.jpg")
+$("h1").text("Bye")
+$("button ").html("<en>i>Key</en>")
+$("a").attr("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ") 
