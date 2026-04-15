45b02fafdb57df83a2b368ad02c67920fc5185fa
stankovvv <mykola.stankov07@gmail.com>
2026-04-15 19:43:46 +0300

    simonGameStart

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,12 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,18 @@
-$("img").attr("src", "https://i.pinimg.com/1200x/68/c0/39/68c0392f474997b4d2937e571cf7a665.jpg")
-$("h1").text("Bye")
-$("button ").html("<en>i>Key</en>")
+$("h1").before("<button>New</button>")
+$("h1").after("<button>New</button>")
+$("h1").prepend("<button>New</button>")
+$("h1").append("<button>New</button>")
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "blue");
+});
+$("button ").html("<en>Key</en>")
 $("a").attr("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ") 
+
+const headerHide= $("button").click(function () {
+    $("h1").fadeToggle();
+    
+});
+$(document).keydown(function (e) {
+    console.log(e.key);
+    $("h1").text(e.key);
+});
