139eafa9af212c9c16350078543e1bd3fa12dfa0
stankovvv <mykola.stankov07@gmail.com>
2026-04-23 19:56:45 +0300

    qr-gen

--- node/2.2 Native Modules/index.js
+++ node/2.2 Native Modules/index.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+
+/* fs.writeFile('hello.txt', 'Hello, World!'+ " Message from Kolyamba! "+ Date.now(), (err) => {
+  if (err) throw err;
+  console.log('File has been saved!');
+}) */
+
+fs.readFile('hello.txt', "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
---
+++ node/2.3 NPM/index.js
@@ -0,0 +1,5 @@
+/* var generateName = require('sillyname');
+var sillyName = generateName(); */
+import generateName from 'sillyname';
+var sillyName = generateName();
+console.log(`Silly Name: ${sillyName}`);
