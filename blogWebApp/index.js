79571e58e99f85d3346f6953004485c2bce54e1c
stankovvv <mykola.stankov07@gmail.com>
2026-05-04 21:59:53 +0300

    herefthyjtykutjyhtgre

--- blogWebApp/index.js
+++ blogWebApp/index.js
@@ -8,12 +8,30 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public/views'));
+app.use(express.static('public'));
+
+function generatePostId() {
+  return posts.length > 0 ? posts[posts.length - 1].id + 1 : 1;
+}
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { posts });
+});
+
+function postPubTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  return `${hours}:${minutes}`;
+}
+app.get('/health', (req, res) => {
+  res.status(200).send('Server is running!');
+});
 
 const posts = []; // In-memory storage for posts
 function addPost(title, content) {
   // Logic to add a new post to the database or in-memory storage
-  const newPost = { id: Date.now(), title, content };
+  const newPost = { id: generatePostId(), title, content, pubTime: postPubTime() };
   posts.push(newPost);
   console.log(`Post added: ${title} - ${content}`);
 }
@@ -27,21 +45,20 @@ function deletePost(postId) {
   }
 }
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { posts });
-});
 
+//add post
 app.post('/submit', (req, res) => {
   const { title, content } = req.body;
   addPost(title, content);
   res.redirect('/');
 });
-
+//delete post
 app.delete('/delete', (req, res) => {
   const { id } = req.body;
   deletePost(id);
   res.redirect('/');
 });
+//edit post
 app.put('/edit', (req, res) => {
   const { id, title, content } = req.body;
   const postIndex = posts.findIndex(post => post.id === parseInt(id));
